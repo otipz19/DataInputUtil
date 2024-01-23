32abfda9914bbc4403804466c285d0017c298bbd
otipz19 <tasitwork@gmail.com>
2024-01-23 11:40:26 +0200

    Added overloaded methods with prompts

--- src/DataInputUtil/ConsoleDataReader.java
+++ src/DataInputUtil/ConsoleDataReader.java
@@ -6,6 +6,41 @@ package DataInputUtil;
 public class ConsoleDataReader {
     private static final StreamDataReader streamDataReader = new StreamDataReader(System.in);
 
+    public static double getNotNegativeNumber(String prompt){
+        System.out.println(prompt);
+        return getNotNegativeNumber();
+    }
+
+    public static double getNegativeNumber(String prompt){
+        System.out.println(prompt);
+        return getNegativeNumber();
+    }
+
+    public static double getDouble(String prompt){
+        System.out.println(prompt);
+        return getDouble();
+    }
+
+    public static long getLong(String prompt) {
+        System.out.println(prompt);
+        return getLong();
+    }
+
+    public static char getChar(String prompt) {
+        System.out.println(prompt);
+        return getChar();
+    }
+
+    public static int getInt(String prompt) {
+        System.out.println(prompt);
+        return getInt();
+    }
+
+    public static String getLine(String prompt) {
+        System.out.println(prompt);
+        return getLine();
+    }
+
     public static double getNotNegativeNumber(){
         return streamDataReader.getNotNegativeNumber();
     }
